d1edaea12548365799c5ee66aa70376e81bba6ea
Marcin Bojara <marcin_5@mail.ru>
2025-04-21 18:15:04 +0200

    Use `wee_alloc` as the global memory allocator.
    Replaced the default memory allocator with `wee_alloc` to reduce the Wasm binary size. This change involves adding the `wee_alloc` dependency and setting it as the global allocator in the project.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
