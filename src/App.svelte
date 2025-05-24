5ec3a17dcea3bf29b935858ebea436231bf207f6
Marcin Bojara <marcin_5@mail.ru>
2025-05-24 05:38:54 +0200

    Add grid rendering functionality to the canvas
    Introduced functions to initialize and draw a grid on a canvas using Svelte. This update includes canvas setup, grid visualization, and binds a canvas element for rendering. The grid dynamically updates based on the world's dimensions.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,64 @@
 <script lang="ts">
+    import {onMount} from 'svelte';
     import {World} from '../pkg';
 
-    const world = World.new();
-    console.log(world.width());
+    const CELL_SIZE = 20;
+    const GRID_COLOR = '#122334';
+    const WORLD_SIZE = 8;
+
+    let world: World;
+    let canvas: HTMLCanvasElement;
+
+    function initializeCanvas(ctx: CanvasRenderingContext2D) {
+        const width = CELL_SIZE * world.width();
+        const height = CELL_SIZE * world.height();
+
+        canvas.width = width;
+        canvas.height = height;
+
+        // Clear the canvas
+        ctx.clearRect(0, 0, width, height);
+    }
+
+    function drawGrid(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.strokeStyle = GRID_COLOR;
+
+        // Draw vertical lines
+        for (let x = 0; x <= world.width(); x++) {
+            ctx.moveTo(x * CELL_SIZE, 0);
+            ctx.lineTo(x * CELL_SIZE, canvas.height);
+        }
+
+        // Draw horizontal lines
+        for (let y = 0; y <= world.height(); y++) {
+            ctx.moveTo(0, y * CELL_SIZE);
+            ctx.lineTo(canvas.width, y * CELL_SIZE);
+        }
+
+        ctx.stroke();
+    }
+
+    onMount(() => {
+        world = World.new(WORLD_SIZE);
+
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            console.error('Could not get 2D context from canvas');
+            return;
+        }
+
+        initializeCanvas(ctx);
+        drawGrid(ctx);
+    });
 </script>
 
+<canvas bind:this={canvas}></canvas>
+
 <main>
-    <h1>Vite + Svelte + WebAssembly</h1>
+    <div class="top-0 left-0 w-full h-full absolute flex items-center justify-center flex-col">
+        <canvas
+                bind:this={canvas}
+        ></canvas>
+    </div>
 </main>
