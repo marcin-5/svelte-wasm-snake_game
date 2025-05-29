0dc2c1412ebdf4a03f4469a14f022f074d64c35d
Marcin Bojara <marcin_5@mail.ru>
2025-05-29 16:50:19 +0200

    Disable game control button based on game status
    Add a conditional `disabled` attribute to the game control button to reflect the game's status. This ensures the button is only clickable when appropriate, improving the user experience and preventing unintended actions.

--- src/App.svelte
+++ src/App.svelte
@@ -115,6 +115,7 @@
             <div class="flex justify-center mt-5">
                 <button bind:this={gameControlBtn}
                         class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-800 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                        disabled={world ? !!world.game_status() : null}
                         id="game-control-btn">
                     Play
                 </button>
