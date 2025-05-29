5596b5a602ae954c6738f395667dfb56d73a7c34
Marcin Bojara <marcin_5@mail.ru>
2025-05-29 14:52:37 +0200

    Update snake body when consuming a reward
    Modify logic to extend the snake's body upon reward consumption. This ensures the game accurately reflects growth mechanics when the snake eats a reward.

--- src/lib.rs
+++ src/lib.rs
@@ -166,5 +166,9 @@ impl World {
         for i in 1..len {
             self.snake.body[i] = SnakeCell(temp[i - 1].0);
         }
+
+        if self.snake_head_idx() == self.reward_cell {
+            self.snake.body.push(SnakeCell(self.snake.body[1].0));
+        }
     }
 }
